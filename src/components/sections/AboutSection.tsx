bb7ddf995a440f24e310024f2877e4438265c458
Julio189 <julio.c.jesus@hotmail.com>
2023-07-09 16:36:50 -0300

    style: add new text color and spaces

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -5,14 +5,16 @@ const AboutSection = () => {
   return (
     <section
       id="about"
-      className="min-h-screen bg-secondaryBgColor text-textColor pt-28 px-[9%] pb-5 items-center"
+      className="min-h-screen bg-secondaryBgColor text-textColor pt-32 px-[9%] pb-5 items-center"
     >
       <div className="flex justify-between">
         <div className="basis-[35%]">
           <h1>Aqui vai a imgaem</h1>
         </div>
         <div className="basis-[60%]">
-          <h1 className="text-4xl font-bold mb-4">Sobre Mim</h1>
+          <h1 className="text-5xl font-bold mb-5">
+            Sobre <span className="text-mainColor">Mim</span>
+          </h1>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
             sed molestiae esse aliquam dolor minima quibusdam cupiditate eos ab

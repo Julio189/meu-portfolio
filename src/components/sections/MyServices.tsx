df3131a86940f2959514dc02eddcaac95fb54777
Julio189 <julio.c.jesus@hotmail.com>
2023-07-18 13:49:12 -0300

    feat: add motion to my services section

--- src/components/sections/MyServices.tsx
+++ src/components/sections/MyServices.tsx
@@ -2,6 +2,7 @@ import ServicesCard from '../cards/ServicesCard'
 import { BsDatabaseCheck } from 'react-icons/bs'
 import { HiCode } from 'react-icons/hi'
 import { FaComputer } from 'react-icons/fa6'
+import { motion } from 'framer-motion'
 
 const MyServices = () => {
   return (
@@ -9,10 +10,30 @@ const MyServices = () => {
       className="bg-primaryBgColor text-textColor min-h-screen pt-28 sm:pt-32 px-[9%] pb-4 md:pb-5 flex flex-col items-center"
       id="serviços"
     >
-      <h3 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-16 md:mb-20">
+      <motion.h3
+        className="text-3xl sm:text-4xl md:text-5xl font-bold mb-16 md:mb-20"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.8 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
         Meus <span className="text-mainColor">Serviços</span>
-      </h3>
-      <div className="grid sm:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-5">
+      </motion.h3>
+      <motion.div
+        className="grid sm:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-5"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+      >
         <ServicesCard
           icon={<HiCode size={50} />}
           title="Desenvolvedor Web"
@@ -28,7 +49,7 @@ const MyServices = () => {
           title="Softwares e Hardwares"
           content="Criação de sites tanto front end como back end, utilizando mais as linguagens javascript e php"
         />
-      </div>
+      </motion.div>
     </section>
   )
 }

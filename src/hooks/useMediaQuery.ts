0991313baba373b5ebe1c379cec0e91b4476e1d4
Julio189 <julio.c.jesus@hotmail.com>
2023-07-24 00:00:12 -0300

    fix: media

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -8,10 +8,11 @@ const useMediaQuery = (query: string) => {
     if (media.matches !== matches) {
       setMatches(media.matches)
     }
+
     const listener = () => setMatches(media.matches)
     window.addEventListener('resize', listener)
     return () => window.removeEventListener('resize', listener)
-  }, [matches, query])
+  }, [matches, media.matches, query])
   return matches
 }
 export default useMediaQuery

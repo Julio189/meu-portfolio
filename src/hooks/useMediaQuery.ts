be7ac84db5aa5be16ea6d10344b1fb5dff2ce927
Julio189 <julio.c.jesus@hotmail.com>
2023-07-24 00:57:40 -0300

    fix: fix code media

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -2,19 +2,18 @@
 import { useState, useEffect } from 'react'
 
 const useMediaQuery = (query: string) => {
-  const media = window.matchMedia(query)
   const [matches, setMatches] = useState(false)
 
   useEffect(() => {
-    if (typeof window === 'undefined') return
-    else if (media.matches !== matches) {
+    const media = window.matchMedia(query)
+    if (media.matches !== matches) {
       setMatches(media.matches)
     }
 
     const listener = () => setMatches(media.matches)
     window.addEventListener('resize', listener)
     return () => window.removeEventListener('resize', listener)
-  }, [matches, media.matches, query])
+  }, [matches, matches, query])
   return matches
 }
 export default useMediaQuery

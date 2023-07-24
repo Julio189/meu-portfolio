be6c15cd20e0cc823486dda5bbe686d293a405bd
Julio189 <julio.c.jesus@hotmail.com>
2023-07-23 23:38:07 -0300

    fix: edit fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
 
-import Header from '@/components/header/Header'
 import AboutSection from '@/components/sections/AboutSection'
 import ContactSection from '@/components/sections/ContactSection'
 import Footer from '@/components/sections/Footer'
@@ -9,12 +8,10 @@ import HomeSection from '@/components/sections/HomeSection'
 import MyServices from '@/components/sections/MyServices'
 import ProjectsSection from '@/components/sections/ProjectsSection'
 
-import useMediaQuery from '@/hooks/useMediaQuery'
 import Navbar from '@/components/header/Navbar'
 
 export default function Home() {
   const [selectedPage, setSelectedPage] = useState('início')
-  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)')
   return (
     <>
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
